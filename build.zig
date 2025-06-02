6498e9db4cda653d7786c6b8f51c20f78f9ffd69
Jacob Crabill <jcrabill@darkhive.com>
2025-06-02 16:14:37 -0700

    add -pic flag

--- build.zig
+++ build.zig
@@ -18,6 +18,7 @@ pub fn build(b: *std.Build) !void {
             .optimize = optimize,
             .link_libc = true,
             .link_libcpp = true,
+            .pic = true,
         }),
         .linkage = linkage,
     });
